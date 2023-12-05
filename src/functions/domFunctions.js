c2f273dd98ac668432106f53df938e0cc84da295
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-12-05 14:35:06 -0600

    Add better notation for each day, logic done

--- src/functions/domFunctions.js
+++ src/functions/domFunctions.js
@@ -4,9 +4,11 @@ const { getCurrentWeather, getForecastWeather } = apiFunctions;
 
 const createCurrentWeatherWidget = async () => {
   const currentWeatherWidget = document.createElement("div");
+  const currentWeatherWidgetTitle = document.createElement("div");
   const currentWeatherWidgetLocation = document.createElement("div");
   const currentWeatherWidgetDate = document.createElement("div");
-  const currentWeatherWidgetTemperature = document.createElement("div");
+  const currentWeatherWidgetTemperatureC = document.createElement("div");
+  const currentWeatherWidgetTemperatureF = document.createElement("div");
   const currentWeatherWidgetCondition = document.createElement("div");
   const currentWeatherWidgetConditionIcon = document.createElement("img");
   const currentWeatherWidgetWind = document.createElement("div");
@@ -16,9 +18,10 @@ const createCurrentWeatherWidget = async () => {
   currentWeatherWidgetLocation.classList.add(
     "current-weather-widget__location"
   );
+  currentWeatherWidgetTitle.classList.add("current-weather-widget__title");
   currentWeatherWidgetDate.classList.add("current-weather-widget__date");
-  currentWeatherWidgetTemperature.classList.add(
-    "current-weather-widget__temperature"
+  currentWeatherWidgetTemperatureC.classList.add(
+    "current-weather-widget__temperatureC"
   );
   currentWeatherWidgetCondition.classList.add(
     "current-weather-widget__condition"
@@ -31,9 +34,11 @@ const createCurrentWeatherWidget = async () => {
     "current-weather-widget__humidity"
   );
 
+  currentWeatherWidget.appendChild(currentWeatherWidgetTitle);
   currentWeatherWidget.appendChild(currentWeatherWidgetLocation);
   currentWeatherWidget.appendChild(currentWeatherWidgetDate);
-  currentWeatherWidget.appendChild(currentWeatherWidgetTemperature);
+  currentWeatherWidget.appendChild(currentWeatherWidgetTemperatureC);
+  currentWeatherWidget.appendChild(currentWeatherWidgetTemperatureF);
   currentWeatherWidget.appendChild(currentWeatherWidgetCondition);
   currentWeatherWidget.appendChild(currentWeatherWidgetConditionIcon);
   currentWeatherWidget.appendChild(currentWeatherWidgetWind);
@@ -41,10 +46,11 @@ const createCurrentWeatherWidget = async () => {
 
   const currentWeatherData = await getCurrentWeather();
 
+  currentWeatherWidgetTitle.textContent = "Current Weather";
   currentWeatherWidgetLocation.textContent = currentWeatherData.location.name;
   currentWeatherWidgetDate.textContent = currentWeatherData.location.localtime;
-  currentWeatherWidgetTemperature.textContent =
-    currentWeatherData.current.temp_c;
+  currentWeatherWidgetTemperatureC.textContent = `${currentWeatherData.current.temp_c}°C`;
+  currentWeatherWidgetTemperatureF.textContent = `${currentWeatherData.current.temp_f}°F`;
   currentWeatherWidgetCondition.textContent =
     currentWeatherData.current.condition.text;
   currentWeatherWidgetConditionIcon.src =
@@ -58,7 +64,13 @@ const createCurrentWeatherWidget = async () => {
 
 const createForecastWeatherWidget = async () => {
   const forecastWeatherWidget = document.createElement("div");
+  const forecastWeatherWidgetTitle = document.createElement("div");
   forecastWeatherWidget.classList.add("forecast-weather-widget");
+
+  forecastWeatherWidgetTitle.classList.add("forecast-weather-widget__title");
+  forecastWeatherWidgetTitle.textContent = "Forecast Weather";
+
+  forecastWeatherWidget.appendChild(forecastWeatherWidgetTitle);
 
   const forecastWeatherData = await getForecastWeather();
   const forecastWeatherDays = forecastWeatherData.forecast.forecastday;
@@ -83,10 +95,9 @@ const createForecastWeatherWidget = async () => {
       "forecast-weather-widget__condition-icon"
     );
 
-    forecastWeatherWidgetDay.textContent = day.date;
     forecastWeatherWidgetDate.textContent = day.date;
-    forecastWeatherWidgetMin.textContent = day.day.mintemp_c;
-    forecastWeatherWidgetMax.textContent = day.day.maxtemp_c;
+    forecastWeatherWidgetMin.textContent = `Min: ${day.day.mintemp_c}°C / ${day.day.mintemp_f}°F`;
+    forecastWeatherWidgetMax.textContent = `Max: ${day.day.maxtemp_c}°C / ${day.day.maxtemp_f}°F`;
     forecastWeatherWidgetCondition.textContent = day.day.condition.text;
     forecastWeatherWidgetConditionIcon.src = day.day.condition.icon;
 
@@ -105,6 +116,7 @@ const createForecastWeatherWidget = async () => {
 
 const createTimeWidget = () => {
   const timeWidget = document.createElement("div");
+  const timeWidgetTitle = document.createElement("div");
   const timeWidgetTime = document.createElement("div");
   const timeWidgetDate = document.createElement("div");
 
@@ -112,6 +124,7 @@ const createTimeWidget = () => {
   timeWidgetTime.classList.add("time-widget__time");
   timeWidgetDate.classList.add("time-widget__date");
 
+  timeWidget.appendChild(timeWidgetTitle);
   timeWidget.appendChild(timeWidgetTime);
   timeWidget.appendChild(timeWidgetDate);
 
@@ -126,6 +139,8 @@ const createTimeWidget = () => {
     timeWidgetTime.textContent = date.toLocaleTimeString();
     timeWidgetDate.textContent = date.toLocaleDateString();
   }, 1000);
+
+  timeWidgetTitle.textContent = "Monterrey, Mexico";
 
   return timeWidget;
 };
